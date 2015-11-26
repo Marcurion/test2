37425035a4db1084b4b03154b16d6cac90c2e31b
Marcurion <marcurion@googlemail.com>
2015-11-26 11:32:37 +0100

    first commit

---
+++ Classes/UI/SecureTextField.h
@@ -0,0 +1,18 @@
+//
+//  SecureTextField.h
+//  Unity-iPhone
+//
+//  Created by Bernd Reicherstorfer on 27.10.15.
+//
+//
+#import <UIKit/UIKit.h>
+
+#ifndef SecureTextField_h
+#define SecureTextField_h
+
+@interface SecureTextField : UITextField
+
+@end
+
+
+#endif /* SecureTextField_h */
---
+++ Classes/UI/SecureTextField.m
@@ -0,0 +1,20 @@
+//
+//  SecureTextField.m
+//  Unity-iPhone
+//
+//  Created by Bernd Reicherstorfer on 27.10.15.
+//
+//
+
+#import "SecureTextField.h"
+
+@implementation SecureTextField
+
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+{
+    return false;
+}
+
+@end
+
+
